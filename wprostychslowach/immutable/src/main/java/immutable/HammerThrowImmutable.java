d87f4958510933b11ebd3529478e654623a59987
wprostychslowach <me@wprostychslowach.pl>
2020-02-06 12:05:24 +0100

    Some explanation

--- wprostychslowach/immutable/src/main/java/immutable/HammerThrowImmutable.java
+++ wprostychslowach/immutable/src/main/java/immutable/HammerThrowImmutable.java
@@ -1,13 +1,18 @@
 package immutable;
 
+/* Our class cannot be extended, so our method can't be overriden */
 final class HammerThrowImmutable {
 
+    /* It's final int varaible - we can't change it after object is created */
     private final int meters;
 
+    /* Init meters variable once. Our object can be in one state. If you pass to constructor mutable object, remember to clone it */
     HammerThrowImmutable(final int meters) {
         this.meters = meters;
     }
 
+
+    /* We create new object to be immutable */
     HammerThrowImmutable add(final int meters) {
         return new HammerThrowImmutable(this.meters+meters);
     }
